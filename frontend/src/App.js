4030e0fb399304084746cc06ef8e805aae2f7ccb
acet09 <ysjungman@gmail.com>
2023-05-24 07:37:21 +0900

    307_useRouteLoaderData

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,3 @@
-// Challenge / Exercise
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-//    - 끝-
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-//    - 끝-
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-//    - 끝 -
-// 4. Add properly working links to the MainNavigation
-//    - 끝 - 
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-//    - 끝 -
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-//    - 끝 -
-// 7. Output the ID of the selected event on the EventDetailPage
-//    - 끝 -
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
-
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import RootLayout from './pages/Root';
@@ -36,7 +8,6 @@ import NewEventPage from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
 import EventsRootLayout from './pages/EventsRoot';
 import ErrorPage from './pages/Error';
-
 
 const router = createBrowserRouter([
   {
@@ -56,12 +27,18 @@ const router = createBrowserRouter([
           },
           {
             path: ':eventId',
-            element: <EventDetailPage />,
+            id: 'event-detail',
             loader: eventDetailLoader,
+            children: [
+              {
+                index: true,
+                element: <EventDetailPage />,
+              },
+              { path: 'edit', element: <EditEventPage /> },
+            ],
           },
           { path: 'new', element: <NewEventPage /> },
-          { path: ':eventId/edit', element: <EditEventPage /> },
-        ]
+        ],
       },
     ],
   },
